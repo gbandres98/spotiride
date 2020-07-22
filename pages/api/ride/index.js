f28862233b02ad8a8c60a761d6333937b9d82b60
Guillermo Bandrés Catalina <mail@gbandres.com>
2020-07-22 18:41:28 +0200

    Use collaborative playlists

--- pages/api/ride/index.js
+++ pages/api/ride/index.js
@@ -7,11 +7,39 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.post(async (req, res) => {
-  const { name } = req.body;
+  const { name, token } = req.body;
+
+  const userData = await axios.get(`https://api.spotify.com/v1/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const userId = userData.data.id;
+
+  const created = await axios.post(
+    `https://api.spotify.com/v1/users/${userId}/playlists`,
+    {
+      name: name,
+      description: `Playlist creada para ${userData.data.display_name} por SpotiRide!`,
+      public: false,
+      collaborative: true,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const playlistId = created.data.id;
+
   let doc = await req.db.collection("rides").insertOne({
     name,
     users: [],
     tracks: [],
+    playlistId: playlistId,
   });
 
   console.log(doc.ops[0]);
